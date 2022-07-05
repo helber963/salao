ee285df852a27d37245b5cdbf3e7d29f08f599e6
Helber Chaves <helber963@gmail.com>
2022-07-05 18:28:56 -0300

    subindo script

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -1,24 +1,24 @@
     let listaServicos = [
         {
-            imagem: '<div class="card-services"><img src="img/scissors-round.jpg" alt="Corte">',
+            imagem: '<div class="card-services"><img src="img/scissors-round.jpg" alt="Corte" >',
             titulo: "CORTE",
             descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
             valor: "R$XX.XX"
         },
         {
-            imagem: '<img src="img/cream-round.png" alt="Hidratação">',
+            imagem: '<img src="img/cream-round.png" alt="Hidratação" >',
             titulo: "HIDRATAÇÃO",
             descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
             valor: "R$XX.XX"
         },
         {
-            imagem: '<img src="img/nails-round.png" alt="Unhas">',
+            imagem: '<img src="img/nails-round.png" alt="Unhas" >',
             titulo: "UNHAS",
             descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
             valor: "R$XX.XX"
         },
         {
-            imagem: '<img src="img/skincare-round.png" alt="Skincare">',
+            imagem: '<img src="img/skincare-round.png" alt="Skincare" >',
             titulo: "SKINCARE",
             descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
             valor: "R$XX.XX"
@@ -34,13 +34,14 @@
         for(let index =0; index < listaServicos.length; index++){
             const servico = listaServicos[index];
             
-            template += `<div class="card-services">${servico.imagem}
+            template += `<div class="card-servicos "></div><div class="card-services">${servico.imagem}
             <h2>${servico.titulo}</h2>
             <p>${servico.descricao}</p>
             <h2>${servico.valor}</h2>
-        </div>`
+        </div></div>`
         }
         espaco.innerHTML = template;
+
 
 
 
