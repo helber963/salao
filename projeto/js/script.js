281b5af290c582d3cbbf8f5e9f69c0b658f799b8
Helber Chaves <helber963@gmail.com>
2022-07-05 14:36:38 -0300

    subindo arquivos

---
+++ projeto/js/script.js
@@ -0,0 +1,48 @@
+    let listaServicos = [
+        {
+            imagem: '<div class="card-services"><img src="img/scissors-round.jpg" alt="Corte">',
+            titulo: "CORTE",
+            descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
+            valor: "R$XX.XX"
+        },
+        {
+            imagem: '<img src="img/cream-round.png" alt="Hidratação">',
+            titulo: "HIDRATAÇÃO",
+            descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
+            valor: "R$XX.XX"
+        },
+        {
+            imagem: '<img src="img/nails-round.png" alt="Unhas">',
+            titulo: "UNHAS",
+            descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
+            valor: "R$XX.XX"
+        },
+        {
+            imagem: '<img src="img/skincare-round.png" alt="Skincare">',
+            titulo: "SKINCARE",
+            descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero natus fugiat maxime.",
+            valor: "R$XX.XX"
+        },
+    ]
+
+
+    function renderizarServicos(){
+        let espaco = document.getElementById("espaco-servicos")
+
+        let template = "";
+
+        for(let index =0; index < listaServicos.length; index++){
+            const servico = listaServicos[index];
+            
+            template += `<div class="card-services">${servico.imagem}
+            <h2>${servico.titulo}</h2>
+            <p>${servico.descricao}</p>
+            <h2>${servico.valor}</h2>
+        </div>`
+        }
+        espaco.innerHTML = template;
+
+
+
+
+    }
